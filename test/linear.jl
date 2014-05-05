0b2b4713f43ea62551fd8688d5c9cce398a5c23b
Chris Fusting <cfusting@gmail.com>
2014-05-05 17:16:10 -0400

    Simple test for linear case

---
+++ test/linear.jl
@@ -0,0 +1,9 @@
+@everywhere using PSGD, Base.Test
+
+x = linspace(1, 100, 100000)
+y = linspace(100, 200, 100000)
+
+res = psgd(.0002, 200, x, y, linear_gradient, linear_hyp)
+
+@test_approx_eq_eps res[1] 98.9898 .0001
+@test_approx_eq_eps res[2] 1.0101 .0001
